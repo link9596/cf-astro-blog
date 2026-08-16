9b47505119b1f05a4d6205e2e9f754e7c563fc7a
link9596 <lk@atlinker.cn>
2026-08-16 11:49:32 +0800

    更新主题

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
 	output: "server",
 	prefetch: {
 		prefetchAll: true,
-		defaultStrategy: "viewport",
+		defaultStrategy: "hover",
 	},
 	adapter: cloudflare({
 		platformProxy: {
